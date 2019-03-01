17cdac939280e53c07f8dd2b0ef1e84c3d8bceca
hyojin17 <chj7960@likelion.org>
2019-03-01 18:51:57 +0900

    앱재사용

--- login/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from imagekit.models import ImageSpecField
-from imagekit.processors import ResizeToFill#크기조정해주는기능
-
-class Pictures(models.Model):
-    text = models.TextField()
-    image = models.ImageField(upload_to="blogimg")
-    image_thumbnail = ImageSpecField(source='image', processors=[ResizeToFill(120, 100)], format='JPEG', options={'quality':60})
